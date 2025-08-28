c29625cf134192560255855e16fe1ed6fd65ee3b
ngkonco <159634942+ngkonco@users.noreply.github.com>
2025-08-28 09:53:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from  Webhook Automation!"
+    return "Hello from  Webhook Automation Inix!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
